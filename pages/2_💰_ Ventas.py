18f8c0f66f9b4cdc9787005723879e8f6425d7bd
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2023-08-16 17:02:15 -0400

    add parcial and total

--- pages/2_💰_ Ventas.py
+++ pages/2_💰_ Ventas.py
@@ -42,6 +42,7 @@ with st.container():
     rut = nombre_rut.get(nombre_ingreso_venta)
     producto_idx = []
     cantidades = []
+    parciales = []
     with st.expander("Cuenta por pagar?"):
         fecha_pago = st.date_input("Fecha de pago", min_value=date.today(), key="Fecha de pago",
                                help="Fecha en la que a más tardar se realizara el pago")
@@ -50,8 +51,21 @@ with st.container():
     productos = st.multiselect("Producto", productos_nombre_id.keys(), key="Producto")
     for producto in productos:
         cantidad = st.number_input("Cantidad " + producto, min_value=1, max_value=50, value=1, key="Cantidad " + producto)
-        producto_idx.append(productos_nombre_id[producto])
+        current_producto_idx = productos_nombre_id[producto]
+        producto_idx.append(current_producto_idx)
         cantidades.append(cantidad)
+        precio = run_query("SELECT precio FROM productos WHERE producto_id = %s", (current_producto_idx,))[0][0]
+        parcial = cantidad * precio
+        parciales.append(parcial)
+
+    if valid_form():
+        # Table with the selected products
+        st.write("### Productos seleccionados")
+        header = ["Producto", "Cantidad", "Precio", "Parcial"]
+        df = pd.DataFrame(zip(productos, cantidades, [run_query("SELECT precio FROM productos WHERE producto_id = %s", (producto_idx,))[0][0] for producto_idx in producto_idx], parciales), columns=header)
+        #add total row
+        df.loc["Total"] = ["", "", "", sum(parciales)]
+        st.table(df)
 
     submitted = st.button("Agregar", use_container_width=True)
 
@@ -72,16 +86,16 @@ if submitted and valid_form():
         with conn.cursor() as cur:
             # Insert the new order and return the id
             cur.execute(
-                "INSERT INTO pedidos (cliente_rut, fecha_pedido, fecha_pago, pedido_anterior_id) VALUES (%s, %s, %s, %s) RETURNING pedido_id",
-                (rut, date.today(), fecha_pago, pedido_anterior_id),    
+                "INSERT INTO pedidos (cliente_rut, fecha_pedido, fecha_pago, pedido_anterior_id, total) VALUES (%s, %s, %s, %s, %s) RETURNING pedido_id",
+                (rut, date.today(), fecha_pago, pedido_anterior_id, sum(parciales)),    
             )
             conn.commit()
             # get the id of the inserted order
             pedido_id = cur.fetchone()[0]
         # Insert the items of the order
-        for producto_id, cantidad in zip(producto_idx, cantidades):
-            run_execute("INSERT INTO items_pedido (pedido_id, producto_id, cantidad) VALUES (%s, %s, %s)",
-                        (pedido_id, producto_id, cantidad))
+        for producto_id, cantidad, parcial in zip(producto_idx, cantidades, parciales):
+            run_execute("INSERT INTO items_pedido (pedido_id, producto_id, cantidad, parcial) VALUES (%s, %s, %s, %s)",
+                        (pedido_id, producto_id, cantidad, parcial))
         # pop default nombre
         if "nombre" in st.session_state:
             st.session_state.pop("nombre")
@@ -97,9 +111,9 @@ with st.expander("Ver ultimas ventas"):
     # pedidos = run_query("SELECT * FROM pedidos LIMIT %s", (num_pedidos,))
     pedidos = run_query("SELECT * FROM pedidos ORDER BY pedido_id DESC LIMIT %s", (num_pedidos,))
     # convert to dataframe
-    header = ["Pedido ID", "RUT", "Fecha Pedido", "Fecha Pago", "Pedido Anterior ID"]
+    header = ["Pedido ID", "RUT", "Fecha Pedido", "Fecha Pago", "Pedido Anterior ID", "Total"]
     pedidos = pd.DataFrame(pedidos, columns=header)
-    atributes = st.multiselect("Atributos a mostrar", header, default=["Pedido ID", "RUT", "Fecha Pedido"])
+    atributes = st.multiselect("Atributos a mostrar", header, default=["Pedido ID", "RUT", "Total", "Fecha Pedido"])
     st.write("## Tabla Pedidos")
     st.table(pedidos[atributes])
     if st.button("Recargar", use_container_width=True):
@@ -111,9 +125,18 @@ with st.expander("Ver items de un pedido"):
     # get the items of the selected order
     items = run_query("SELECT * FROM items_pedido WHERE pedido_id = %s", (pedido_id,))
     # convert to dataframe
-    header = ["Item Pedido ID", "Pedido ID", "Producto ID", "Cantidad"]
+    header = ["Item Pedido ID", "Pedido ID", "Producto ID", "Cantidad", "Parcial"]
     items = pd.DataFrame(items, columns=header)
-    atributes = st.multiselect("Atributos a mostrar", header, default=["Producto ID", "Cantidad"])
+    # add producto column
+    productos = run_query("SELECT producto_id, nombre FROM productos")
+    productos_nombre_id = {producto_id: nombre for producto_id, nombre in productos}
+    items["Producto"] = items["Producto ID"].apply(lambda x: productos_nombre_id.get(x))
+    header.append("Producto")
+    # add total row
+    total = items["Parcial"].sum()
+    items.loc["Total"] = ["", "", "", "", "", ""]
+    items.loc["Total", "Parcial"] = total
+    atributes = st.multiselect("Atributos a mostrar", header, default=["Producto", "Cantidad", "Parcial"])
     st.write("## Tabla Items")
     st.table(items[atributes])
 
